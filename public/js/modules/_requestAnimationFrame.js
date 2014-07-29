d66e0ba4c3c91b1539d18e8a6f6038701d931df3
bnhovde <bnhovde@gmail.com>
2014-07-29 18:39:14 +0100

    Added fps to settings

--- public/js/modules/_requestAnimationFrame.js
+++ public/js/modules/_requestAnimationFrame.js
@@ -1,15 +1,19 @@
-// shim layer with setTimeout fallback by Paul Irih
+define(['settings'], function (s) {
 
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       || 
-          window.webkitRequestAnimationFrame || 
-          window.mozRequestAnimationFrame    || 
-          window.oRequestAnimationFrame      || 
-          window.msRequestAnimationFrame     || 
-          function(/* function */ callback, /* DOMElement */ element){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
+	// shim layer with setTimeout fallback by Paul Irih
+
+	var fps = s.fps;
+
+	window.requestAnimFrame = (function(){
+	  return  window.requestAnimationFrame       || 
+	          window.webkitRequestAnimationFrame || 
+	          window.mozRequestAnimationFrame    || 
+	          window.oRequestAnimationFrame      || 
+	          window.msRequestAnimationFrame     || 
+	          function(/* function */ callback, /* DOMElement */ element){
+	            window.setTimeout(callback, 1000 / fps);
+	          };
+	})();
 
 // usage:
 // instead of setInterval(render, 16) ....
@@ -21,3 +25,5 @@ window.requestAnimFrame = (function(){
 
 // place the rAF *before* the render() to assure as close to
 // 60fps with the setTimeout fallback.
+
+});
--- public/js/modules/_settings.js
+++ public/js/modules/_settings.js
@@ -7,11 +7,12 @@ define([], function () {
 	**************************************************/
 
 	var settings = {
-		serverIP: 		'10.0.0.14',	// IP address of node server
+		serverIP: 		'10.0.0.14',	// IP address of server (or localhost for dev)
 		canvasWidth: 	400, 		// Width in px
 		canvasHeight: 	400,		// Height in px
 		gridSquares: 	100,		// Number of units in grid
-		canvasUnit: 	0			// One canvas unit in px (calculated bwith gridsize)
+		canvasUnit: 	0,			// One canvas unit in px (calculated/w gridsize)
+		fps: 			60 			// Frames Per Second
 	};
 
 	return settings;
